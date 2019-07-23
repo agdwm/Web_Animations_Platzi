ab92831edea9cd13371c982528ff1828182e69fa
agdwm <almudenaguerraswm@gmail.com>
2019-07-23 13:41:44 +0200

    feature: created React Animation project 'Invie'

---
+++ 12_project_invie/src/Invie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/invie.css';
+import Portada from './components/Portada';
+import Guitarras from './components/Guitarras';
+import Footer from './components/Footer';
+
+class Invie extends Component {
+  render() {
+    return (
+      <section className='Invie'>
+        <Portada />
+        <Guitarras />
+        <Footer />
+      </section>
+    );
+  }
+}
+
+export default Invie;
